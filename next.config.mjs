906e23664d38724b26bcf42f74bba6fc6ecff663
Fernando Braz <hello@fernandobraz.online>
2025-05-07 04:07:25 +0100

    tries to prevent cloudflare sockets (3)

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,14 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
-  webpack: (config, { isServer }) => {
-    // Handle Cloudflare sockets issue
-    if (isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        'cloudflare:sockets': false,
-      }
-    }
+  webpack: (config, { webpack }) => {
+    // Handle Cloudflare sockets issue, see: https://github.com/vercel/next.js/discussions/50177
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+      }),
+    )
+
     return config
   },
 }
